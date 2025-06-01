7e8b067a48a64d2403b7ebc49b2b3ae60c0c89da
Vineeth Sai Narajala <abcvineeth.sai@gmail.com>
2025-06-01 15:51:30 -0400

    gh-pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/owasp-top-10" element={<OwaspTop10Page />} />
