aad044473733d2ac23eac5f62b3b9e7289469baa
Vineeth Sai Narajala <abcvineeth.sai@gmail.com>
2025-06-02 02:05:31 -0400

    fixed website

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/agentic-ai-prism-shield/' : '/',
+  base: mode === 'production' ? '/aivss/' : '/',
   build: {
     outDir: "docs",
   },
